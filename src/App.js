8c8a7fb27b753248b479815d2b42af13905b88f4
Benson Ndiwa <bnkimtai@gmail.com>
2024-07-22 19:09:34 +0300

    Streamer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Streamer from "./Streamer";
 import MySVG from './images/play.svg';
 
