3e653fe42a6f99c53df3fe508130ca5e8ae3dd4b
Benson Ndiwa <bnkimtai@gmail.com>
2024-07-22 18:57:42 +0300

    Streamer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Streamer from "./Streamer";
+import MySVG from './images/play.svg';
 
 function App() {
   return (
     <div className="App min-h-screen bg-zinc-800">
-      <div className="bg-zinc-800 p-5 w-full text-yellow-300 text-bold">IKO NINI STREAMER</div>
+      <div className="bg-zinc-800 p-5 w-full text-yellow-300 text-bold flex justify-center items-center">
+        <img src={MySVG} alt="My SVG" className='max-h-10' />
+        <span>IKO NINI STREAMER</span>
+      </div>
         <Routes>
           <Route path="/streamer/:vid_id" element={<Streamer />} />
         </Routes>
--- src/Streamer.js
+++ src/Streamer.js
@@ -1,15 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import MuxPlayer from "@mux/mux-player-react";
 import { useParams } from 'react-router-dom';
+import FadeLoader from "react-spinners/FadeLoader";
 
 function Streamer() {
-    const { param1 } = useParams();
+    const { token } = useParams();
+    const [playbackId, setPlaybackId] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(()=>{
+        fetch(`${process.env.REACT_APP_API_UL}/get_stream_data?token=${token}`)
+        .then(res => res.json())
+        .then(data => {
+            setPlaybackId(data.playbackId)
+            setLoading(false);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    },[])
+
   return (
     <div>
-      <MuxPlayer
-      playbackId={param1}
-      accentColor="#ffcc00"
-      ></MuxPlayer>
+        { 
+        
+        loading ? 
+        <div className='flex justify-center mt-20'>     
+            <FadeLoader
+                color={"#ffcc00"}
+            ></FadeLoader>
+        </div> 
+        : 
+        <MuxPlayer
+        playbackId={playbackId}
+        accentColor="#ffcc00"
+        ></MuxPlayer> 
+      }
     </div>
   )
 }
